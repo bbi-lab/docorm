8a58348d0926535d030888b4fe895a7fc91ab522
David Reinhart <davidrr@uw.edu>
2024-07-11 11:34:01 -0700

    Keep all parentIds since it is a hierarchical list of ancestors, and repeated values are valid.

--- src/dao.ts
+++ src/dao.ts
@@ -389,7 +389,6 @@ const makeDao = async function(entityType: EntityType, options: DaoOptionsInput 
           FetchOptions
       const collection = _.last(parentCollections)
       if (collection && parentDaos.length > 0 && parentIds.length > 0) {
-        parentIds = _.uniq(parentIds)
         switch (collection.persistence) {
           case 'inverse-ref': {
             // TODO Use the schema's foreign key path instead of having one in the REST collection config.
@@ -495,7 +494,6 @@ const makeDao = async function(entityType: EntityType, options: DaoOptionsInput 
       ids = _.uniq(ids)
       const collection = _.last(parentCollections)
       if (collection && parentDaos.length > 0 && parentIds.length > 0) {
-        parentIds = _.uniq(parentIds)
         switch (collection.persistence) {
           case 'inverse-ref':
             // TODO
@@ -533,7 +531,6 @@ const makeDao = async function(entityType: EntityType, options: DaoOptionsInput 
     fetchOneById: async function(id: Id, parentIds = [], {client = null, propertyBlacklist = []} = {}) {
       const collection = _.last(parentCollections)
       if (collection && parentDaos.length > 0 && parentIds.length > 0) {
-        parentIds = _.uniq(parentIds)
         switch (collection.persistence) {
           case 'inverse-ref':
             // TODO Ensure that the item belongs to the parent's collection.
